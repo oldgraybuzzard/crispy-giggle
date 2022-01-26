1bf0705c057b6894896038fbbfe9f2c7eda454b2
Chris Zichko <chriszichkocoding@gmail.com>
2022-01-26 08:25:22 -0500

    Entered comments on the emailValidator function for ease of understanding.

--- client/src/utils/emailValidator.js
+++ client/src/utils/emailValidator.js
@@ -1,5 +1,8 @@
+// create a function to grab an email parameter
 function emailValidator(email) {
+    // use regex to match key components of an email address `xxx@xx.xxx`
     const regex = /.+@.+\..+/;
+    // return a test that will give us true or false.
     return regex.test(email);
 };
 
