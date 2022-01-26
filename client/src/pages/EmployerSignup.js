f305175e240c842baa1d1a6886d96a0162473b66
beamchristian <ckbeamcoding@gmail.com>
2022-01-26 08:40:33 -0500

    front end only refactor

--- client/src/pages/EmployerSignup.js
+++ client/src/pages/EmployerSignup.js
@@ -1,42 +1,78 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 
-// import for using mutation once apollo is setup
-import { useMutation } from '@apollo/client';
+// import { Link } from 'react-router-dom';
 
-// imports for Auth and Destructured ADD_EMPLOYER mutation once graphQl backend is setup
-import Auth from '../utils/auth';
-import { ADD_EMPLOYER } from '../utils/mutations';
+// // import for using mutation once apollo is setup
+// import { useMutation } from '@apollo/client';
 
-function EmployerSignup(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+// // imports for Auth and Destructured ADD_EMPLOYER mutation once graphQl backend is setup
+// import Auth from '../utils/auth';
+// import { ADD_EMPLOYER } from '../utils/mutations';
 
-  // variable for when GraphQl mutations is setup
-  const [addEmployer] = useMutation(ADD_EMPLOYER);
+function EmployerSignup() {
+  const [formState, setFormState] = useState({
+    company: '',
+    email: '',
+    password: '',
+  });
+  const [errorMessage, setErrorMessage] = useState('');
+  const { company, email, message } = formState;
+
+  // temporary front end submit
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
+    }
+  };
+
+  const handleChange = e => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log('Handle Form', formState);
+    }
+  };
+
+  // // variable for when GraphQl mutations is setup
+  // const [addEmployer] = useMutation(ADD_EMPLOYER);
 
   // Logic for mutations once backend GraphQl apollo server is setup
-  const handleFormSubmit = async event => {
-    event.preventDefault();
-    const mutationResponse = await addEmployer({
-      variables: {
-        email: formState.email,
-        password: formState.password,
-        companyName: formState.companyName,
-      },
-    });
+  // const handleFormSubmit = async event => {
+  //   event.preventDefault();
+  //   const mutationResponse = await addEmployer({
+  //     variables: {
+  //       email: formState.email,
+  //       password: formState.password,
+  //       companyName: formState.companyName,
+  //     },
+  //   });
 
-    // passing token through the mutation once token and Auth is setup
-    const token = mutationResponse.data.addEmployer.token;
-    Auth.login(token);
-  };
+  //   // passing token through the mutation once token and Auth is setup
+  //   const token = mutationResponse.data.addEmployer.token;
+  //   Auth.login(token);
+  // };
 
-  const handleChange = event => {
-    const { name, value } = event.target;
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
+  // const handleChange = event => {
+  //   const { name, value } = event.target;
+  //   setFormState({
+  //     ...formState,
+  //     [name]: value,
+  //   });
+  // };
 
   return (
     <div>
