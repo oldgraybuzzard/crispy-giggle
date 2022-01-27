7ddc96cd4c666d92c6eb834b81fcb20f9a3722b1
sunita persad <sunitapersad020@gmail.com>
2022-01-26 21:30:04 -0500

    schema files and setup of graphql and apollo

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const { User } = require('./models');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/trainingdb', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.set('useCreateIndex', true);
+mongoose.set('debug', true);
+
+app.post('/submit', ({ body }, res) => {
+  const user = new User(body);
+
+  User.create(user)
+    .then(dbUser => {
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get('/users', (req, res) => {
+  User.find({}).then(users => {
+    res.json(users);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
