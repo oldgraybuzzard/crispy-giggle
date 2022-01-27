812ea9aba6a791e2cfbe17c173b8e94e52cded42
Chris Zichko <chriszichkocoding@gmail.com>
2022-01-27 11:49:28 -0500

    Added dateFormatter.js from the client/utils into the server/utils and for now Course model and Employee model are finished.

--- server/models/Course.js
+++ server/models/Course.js
@@ -1,3 +1,32 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require ('../utils/dateFormatter');
 
+const courseSchema = new Schema({
+    courseText: {
+        type: String,
+        required: "You haven't entered any course material!",
+        minlength: 1,
+        // maxlength can change just leaving it here for now.
+        maxlength: 2000
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: timestamp => dateFormat(timestamp)
+    },
+    employer: {
+        type: String,
+        required: true
+    },
+    employees: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Employee'
+        }
+    ]
+});
+
+
+const Course = model('Course', courseSchema);
 
 module.exports = Course;
