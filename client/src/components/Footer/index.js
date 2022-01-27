1e07fe808779a20c1cbd97647009b366b2d88310
beamchristian <ckbeamcoding@gmail.com>
2022-01-27 15:41:32 -0500

    refactor

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
 function Footer() {
-    function fullYear() {
-        const yearFormat = new Date();
-        return yearFormat.getFullYear();
-    };
+  function fullYear() {
+    const yearFormat = new Date();
+    return yearFormat.getFullYear();
+  }
 
-    return (
-        <footer className='foot-Container'>
-            <h2 className='copyright'>&copy; <span>{fullYear()}</span> Crispy Gigglers</h2>
-            <a href='/' className='footer-contact'>Contact Us</a>
-        </footer>
-    );
-};
+  return (
+    <footer className="foot-Container">
+      <h2 className="copyright">
+        &copy; <span>{fullYear()}</span> Crispy Gigglers
+      </h2>
+      <a href="/" className="footer-contact">
+        Contact Us
+      </a>
+    </footer>
+  );
+}
 
 export default Footer;
