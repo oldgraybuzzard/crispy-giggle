5eb8ad58ad24bdb7120400d65819548dbe2de884
Chris Zichko <chriszichkocoding@gmail.com>
2022-01-28 22:13:41 -0500

    Server is now up and running. Time for typeDefs and mutations

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,10 @@
-//connection.js//
-
 const mongoose = require('mongoose');
 
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://trainingdb', {
-  //useFindAndModify: false,//
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/trainingdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  //useCreateIndex: true,//
+  // useCreateIndex: true,
+  // useFindAndModify: false,
 });
+
 module.exports=mongoose.connection
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,5 +7,38 @@ module.exports = {
   signToken: function({ email, _id }) {
     const payload = { email, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  }
+  },
+
+  // function for authenticated routes
+  authMiddleware: function ({ req }) {
+    // token can be sent via req.query, req.headers, or req.body
+    let token = req.query.token || req.headers.token || req.headers.authorization;
+
+    // if token is req.headers.authorization we need to 
+    // extract the token value from ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token
+        // split the token which will give us the value after the split
+        .split(' ')
+        // pop that value out of the list
+        .pop()
+        // trim any whitespace off the value popped.
+        .trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    // verify token and retrieve user data out of it
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid Tolkien... I mean Invalid Token ;P');
+    }
+
+    // return the updated request
+    return req;
+  },
 };
