e4cedd222d61a25d00d5c8ccac24f183124c105d
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-01-30 07:50:28 -0500

    auth changes

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+import React from 'react';
+
 import decode from 'jwt-decode';
 
 class AuthService {
@@ -44,6 +47,9 @@ class AuthService {
 }
 
 export default new AuthService();
+=======
+import React from 'react';
+>>>>>>> 5ca73fb66849fba62e095abd9501c9562336ec5a
 =======
 import React from 'react';
 >>>>>>> 5ca73fb66849fba62e095abd9501c9562336ec5a
