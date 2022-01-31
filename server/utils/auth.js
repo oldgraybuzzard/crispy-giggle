bb40b5a8c2c68b06a1329972cd4388c683a1eb8a
Chris Zichko <chriszichkocoding@gmail.com>
2022-01-31 12:22:59 -0500

    updated signToken to store companyName as well.

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,8 +4,8 @@ const secret = 'supersupersecret';
 const expiration = '4h';
 
 module.exports = {
-  signToken: function({ email, _id }) {
-    const payload = { email, _id };
+  signToken: function({ companyName, email, _id }) {
+    const payload = { companyName, email, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 
