e1c017c331d52709c98b24e3e209ed46661a653d
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-01-31 10:10:03 -0500

    pulled in develop

---
+++ server/models/Employer.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const { Schema, model } = mongoose;
+
+const employerSchema = new Schema({
+    companyName: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    courses: [
+        {
+            type: Schema.Types.ObjectId,
+            required: 'Course',
+        }
+    ],
+    employees: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Employee'
+        }
+    ]
+});
+
+// set up pre-save middleware to create password
+employerSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+
+// compare the incoming password with the hashed password
+employerSchema.methods.isCorrectPassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+const Employer = model('Employer', employerSchema);
+
+module.exports = Employer;
