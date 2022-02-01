bb40319c1a3fd056b3e55d0c72fb46f83f2be629
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-01-31 20:31:57 -0500

    client reducers, queries, mutations, GlobalState??? validation required

---
+++ client/src/utils/queries.js
@@ -0,0 +1,61 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_EMPLOYERS = gql`
+  {
+    employers {
+      _id
+      companyName
+      courses
+      employees {
+        _id
+        firstName
+        lastName
+        email
+        department
+        role
+        courses {
+          _id
+          courseText
+        }
+      }
+    }
+  `;
+
+export const QUERY_EMPLOYEES = gql`
+  {
+    employees {
+      _id
+      firstName
+      lastname
+      email
+      department
+      role
+      courses {
+        _id
+        courseText
+      }
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      email
+      courses {
+        _id
+        courseText
+      }
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($employers: [ID]!) {
+    checkout(employers: $employers) {
+      session
+    }
+  }
+`;
