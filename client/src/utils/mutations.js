841d6878ffbc6a921dec5104d1b83a8049e05f91
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-02-02 08:34:52 -0500

    update client/mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,39 +1,16 @@
 import { gql } from '@apollo/client';
 
-<<<<<<< HEAD
-export const LOGIN = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-=======
 export const EMPLOYER_LOGIN = gql`
   mutation EmployerLogin($email: String!, $password: String!) {
     employerLogin(email: $email, password: $password) {
       token
       employer {
->>>>>>> 02d62d02c2f71ef734cf725ed121a945617f60ec
         _id
       }
     }
   }
 `;
 
-<<<<<<< HEAD
-export const ADD_ORDER = gql`
-  mutation addOrder($products: [ID]!) {
-    addOrder(products: $products) {
-      purchaseDate
-      products {
-        _id
-        name
-        description
-        price
-        quantity
-        category {
-          name
-        }
-=======
 export const EMPLOYEE_LOGIN = gql`
   mutation EmployeeLogin($email: String!, $password: String!) {
     employeeLogin(email: $email, password: $password) {
@@ -52,41 +29,25 @@ export const ADD_EMPLOYER = gql`
       employer {
         _id
         companyName
->>>>>>> 02d62d02c2f71ef734cf725ed121a945617f60ec
       }
     }
   }
 `;
 
-<<<<<<< HEAD
-export const ADD_USER = gql`
-  mutation addUser(
-=======
 export const ADD_EMPLOYEE = gql`
   mutation AddEmployee(
->>>>>>> 02d62d02c2f71ef734cf725ed121a945617f60ec
     $firstName: String!
     $lastName: String!
     $email: String!
     $password: String!
-<<<<<<< HEAD
-  ) {
-    addUser(
-=======
     $department: String!
     $role: String!
   ) {
     addEmployee(
->>>>>>> 02d62d02c2f71ef734cf725ed121a945617f60ec
       firstName: $firstName
       lastName: $lastName
       email: $email
       password: $password
-<<<<<<< HEAD
-    ) {
-      token
-      user {
-=======
       department: $department
       role: $role
     ) {
@@ -97,20 +58,8 @@ export const ADD_EMPLOYEE = gql`
       department
       role
       employerId {
->>>>>>> 02d62d02c2f71ef734cf725ed121a945617f60ec
         _id
       }
     }
   }
 `;
-<<<<<<< HEAD
-
-export const ADD_EMPLOYEE = gql`
-  mutation addEmployee(
-    $firstName: String!
-    $lastName: String!
-    $email: String!
-  )
-`;
-=======
->>>>>>> 02d62d02c2f71ef734cf725ed121a945617f60ec
